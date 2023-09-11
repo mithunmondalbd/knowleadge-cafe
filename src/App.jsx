5de76ad1c0e30db03985f1575b723919888eae66
mithunmondalbd <mithunhpwin1@gmail.com>
2023-09-12 03:04:54 +0800

    This project should be knowleadge cafe

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from './components/header/Header'
+import './App.css'
+import Blogs from './components/header/blogs/blogs'
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Blogs></Blogs>
+    </>
+  )
+}
+
+export default App
